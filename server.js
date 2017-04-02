ff8416763734c271deae1cf6fbd562e74330816f
babusuriyanarayanan <babusuriyanarayanan@gmail.com>
2017-04-02 18:43:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 
+var fs = require('fs');
+var jsonData = JSON.parse(fs.readFileSync('document.json', 'utf8'));
+
 module.exports = {
   app: function () {
     const app = express()
@@ -9,6 +12,11 @@ module.exports = {
 
     app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
+    
+    app.get('/data', function (req, res) {
+				res.json(jsonData);
+
+	});
 
     return app
   }
